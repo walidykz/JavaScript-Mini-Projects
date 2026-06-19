621c22a1503602b522e714da6bc1fd59a836cf64
Walid2 <bedri.cab@gmail.com>
2026-06-19 16:04:53 +0100

    Number Guessing Game V1

--- Number Guessing Game/index.js
+++ Number Guessing Game/index.js
@@ -1,7 +1,28 @@
+const Min = 1;
+const Max = 100;
+const randomNumber = Math.floor(Math.random() * (Max - Min + 1)) + Min;
+const discription = document.getElementById("discription");
+const myResult = document.getElementById("myResult");
+const myAttempts = document.getElementById("myAttempts");
+let attempts = 0;
 
-function checkMyNumber(){
-    const Min = 1;
-    const Max = 100;
-    const randomNumber = Math.floor(Math.random() * (Max - Min + 1));
-    console.log(randomNumber);
+discription.textContent = `Enter a Number between ${Min} & ${Max} : `;
+function checkGuess(){
+    const myGuess = Number(document.getElementById("myGuess").value);
+    
+    if(myGuess < Min || myGuess > Max){
+        myResult.textContent = `Enter a Number between ${Min} & ${Max}`
+    }else if(myGuess < randomNumber){
+        myResult.textContent = "to Low!"
+        attempts++;
+        myAttempts.textContent = `Your attempts : ${attempts}`;
+    }else if(myGuess > randomNumber){
+        myResult.textContent = "to High!"
+        attempts++;
+        myAttempts.textContent = `Your attempts : ${attempts}`;
+    }else{
+        attempts++;
+        myResult.textContent = `Congradulation! You just find the Correct Number : ${randomNumber}, After ${attempts} attempts.`;
+        myAttempts.textContent = "";
+    }
 }
